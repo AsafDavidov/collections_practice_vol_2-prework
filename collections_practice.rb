190f1b414f08733583946efc5e8edbb8649b5d90
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 21:53:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,7 +46,8 @@ def merge_data(s_keys,data)
       names.each do |personal_info|
         #merged_data << personal_info[0]
           if first_names[:first_name] == personal_info[0]
-            merged_hash = {:first_name => personal_info[0], personal_info[1]}
+            merged_hash = personal_info[1]
+            merged_hash[:first_name] => personal_info[0], personal_info[1]}
             merged_data << merged_hash
           end
       end
