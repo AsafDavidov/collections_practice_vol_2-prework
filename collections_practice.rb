0c93746968b147f7ca02ccd03965d50fe1cc798a
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 21:17:38 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,7 +42,7 @@ def merge_data(s_keys,data)
   merged_data = []
   s_keys.each do |first_names|
     data.each do |names, info|
-      merged_data << names
+      merged_data << names[:awesomeness]
       #if first_names[:first_name] == names
        # merged_data << 1 
       #end
